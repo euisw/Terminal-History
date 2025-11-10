3580aa864152a8a5e32023cda4e017de9362260a
euisw <khr200522@gmail.com>
2025-11-10 16:44:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var web1 = {
+  openHistory : function() {
+    window.open('History/web1_History.html', '_self');
+  }
+}
+var EMC = {
+  openHistory : function() {
+    window.open('History/Encyclopedia_of_Mysterious_Characters_History.html', '_self');
+  }
+}
+var Pages = {
+  openHistory : function() {
+    window.open('History/Pages_History.html', '_self');
+  }
+}
+var TerminalHistory = {
+  openHistory : function() {
+    window.open('History/Terminal & History_History.html', '_self')
+  },
+  openIndex : function() {
+    window.open('/index.html', '_self');
+  }
+}
